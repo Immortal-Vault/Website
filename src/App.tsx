7afc71d99728989ea552596944ba8a853d6bd385
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-08-18 00:32:42 +0300

    feat: errors handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,39 @@ import SignIn from './views/auth/SignIn.tsx'
 import { AuthProvider } from './stores/AuthContext.tsx'
 import { ToastContainer, Zoom } from 'react-toastify'
 import Primary from './views/primary/Primary.tsx'
+import { ErrorBoundary, ErrorBoundaryError } from './errors'
 
 const theme = createTheme({})
 
 export default function App() {
   return (
-    <MantineProvider theme={theme} defaultColorScheme={'dark'}>
-      <ToastContainer
-        position='top-center'
-        autoClose={2500}
-        limit={3}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='dark'
-        transition={Zoom}
-      />
-      <AuthProvider>
-        <HashRouter>
-          <Routes>
-            <Route path={ROUTER_PATH.ROOT} element={<SignIn />} />
-            <Route path={ROUTER_PATH.SIGN_IN} element={<SignIn />} />
-            <Route path={ROUTER_PATH.SIGN_UP} element={<SignUp />} />
-            <Route path={ROUTER_PATH.MAIN_MENU} element={<Primary />} />
-          </Routes>
-        </HashRouter>
-      </AuthProvider>
-    </MantineProvider>
+    <ErrorBoundary fallback={ErrorBoundaryError}>
+      <MantineProvider theme={theme} defaultColorScheme={'dark'}>
+        <ToastContainer
+          position='top-center'
+          autoClose={2500}
+          limit={3}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='dark'
+          transition={Zoom}
+        />
+        <AuthProvider>
+          <HashRouter>
+            <Routes>
+              <Route path={ROUTER_PATH.ROOT} element={<SignIn />} />
+              <Route path={ROUTER_PATH.SIGN_IN} element={<SignIn />} />
+              <Route path={ROUTER_PATH.SIGN_UP} element={<SignUp />} />
+              <Route path={ROUTER_PATH.MAIN_MENU} element={<Primary />} />
+            </Routes>
+          </HashRouter>
+        </AuthProvider>
+      </MantineProvider>
+    </ErrorBoundary>
   )
 }
