789414f395682e46251868668588fac327fcc333
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-08-20 00:38:57 +0300

    feat: primary app shell

--- src/views/primary/Primary.tsx
+++ src/views/primary/Primary.tsx
@@ -1,8 +1,9 @@
-import { Title } from '@mantine/core'
+import { AppShell, Burger, Group, Image, Skeleton } from '@mantine/core'
 import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ROUTER_PATH } from '../../shared/constants.ts'
 import { AuthContext } from '../../stores/AuthContext.tsx'
+import { useDisclosure } from '@mantine/hooks'
 
 export default function Primary() {
   const navigate = useNavigate()
@@ -14,9 +15,37 @@ export default function Primary() {
     }
   }, [context.authState])
 
+  const [opened, { toggle }] = useDisclosure()
+
   return (
-    <div>
-      <Title order={2}>Main View</Title>
-    </div>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding='md'
+    >
+      <AppShell.Header>
+        <Group h='100%' px='md'>
+          <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+          <Image
+            src={'/logo.png'}
+            width={'40px'}
+            height={'40px'}
+            style={{
+              marginLeft: '75%',
+            }}
+            alt={'Immortal Vault'}
+          />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p='md'>
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt='sm' animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
   )
 }
