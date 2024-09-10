375bb993a8f32cf27f9874dfc4e1ff31df1bdd4a
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-09-10 16:02:43 +0300

    chore: resolve todo comment

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -4,7 +4,7 @@ import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 const loadPath = '/locales/{{lng}}/{{ns}}.json'
-// TODO: get current language from database
+
 i18next
   .use(LanguageDetector)
   .use(Backend)
